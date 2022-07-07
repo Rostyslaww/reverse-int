e2740aea70290292c642fdac5449bd98c73a2fbb
Rostyslaw <rostyksamchenko@gmail.com>
2022-07-07 10:59:57 +0300

    init: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const reveredNumber = n.toString().split('').reverse().join('')
+        const rn = parseInt(reveredNumber)*Math.sign(n)
+            const result = Math.abs(rn)
+                return result
 }
